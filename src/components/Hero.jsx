1bcf5f03009eb575f21ee55850827cb8408a7086
Sirihegde02 <118575770+Sirihegde02@users.noreply.github.com>
2024-08-20 23:17:07 -0400

    Made final changes to Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Suspense } from 'react';
 import styled from 'styled-components'
 import Navbar from './Navbar'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 const Section = styled.div`
     height: 100vh;
@@ -9,14 +11,25 @@ const Section = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
+
+    @media only screen and (max-width: 768px) {
+      height: 200vh;
+    }
 `;
 
 const Container = styled.div`
-    height: 100vh;
+    height: 100%;
     scroll-snap-align: center;
     width: 1400px;
     display: flex;
     justify-content: space-between;
+
+    @media only screen and (max-width: 768px) {
+      width: 100%;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
 `;
 
 const Left = styled.div`
@@ -25,10 +38,19 @@ const Left = styled.div`
     flex-direction: column;
     justify-content: center;
     gap: 20px;
+
+    @media only screen and (max-width: 768px) {
+      flex: 1;
+      align-items: center;
+    }
 `;
 
 const Title = styled.h1`
-    font-size: 40px;
+    font-size: 50px;
+
+    @media only screen and (max-width: 768px) {
+      text-align: center;
+    }
 `;
 
 const WhatWeDo = styled.div`
@@ -39,7 +61,6 @@ const WhatWeDo = styled.div`
 
 const Line = styled.img`
     height: 5px;
-
 `;
 
 const Subtitle = styled.h2`
@@ -49,6 +70,11 @@ const Subtitle = styled.h2`
 const Desc = styled.p`
     font-size: 24px;
     color: lightgray;
+
+    @media only screen and (max-width: 768px) {
+      padding: 20px;
+      text-align: center;
+    }
 `;
 
 const Button = styled.button`
@@ -87,11 +113,16 @@ const Button = styled.button`
 const Right = styled.div`
     flex: 3;
     position: relative;
+
+    @media only screen and (max-width: 768px) {
+      flex: 1;
+      width: 100%;
+    }
 `;
 
 const Img = styled.img`
-    width: 600px;
-    height; 600px;
+    width: 800px;
+    height: 600px;
     object-fit: contain;
     position: absolute;
     top: 0;
@@ -101,34 +132,55 @@ const Img = styled.img`
     margin: auto;
     animation: animate 2s infinite ease alternate;
 
-    @keyframes animate{
-    to{
-    transform: translateY(29px);
+    @media only screen and (max-width: 768px) {
+      width: 300px;
+      height: 300px;
     }
+
+    @keyframes animate {
+      to {
+        transform: translateY(20px);
+      }
     }
 `;
 
 const Hero = () => {
   return (
     <Section>
-      <Navbar/>
+      <Navbar />
       <Container>
         <Left>
-          <Title>Hi I'm Siri, and welcome to my portfolio website!</Title>
+          <Title>Hey there! Welcome to my portfolio website!</Title>
           <WhatWeDo>
-            <Line src = "./img/line.png"/>
-            <Subtitle>Who am I</Subtitle>
+            <Line src="./img/line.png" />
+            <Subtitle>My name is Siri Hegde.</Subtitle>
           </WhatWeDo>
-          <Desc>I am a student at Penn State University majoring in Computer Science, with a minor in Mathematics.</Desc>
+          <Desc>
+          I am a student at Penn State University majoring in Computer Science, with a minor in Mathematics.
+          </Desc>
           <Button>Learn More</Button>
         </Left>
         <Right>
-          {/* 3d model */}
-          <Img src = "/img/spaceman.png"/>
+          <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#3d1c56"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
+          <Img src="./img/spaceman.png" />
         </Right>
       </Container>
     </Section>
   );
 };
 
-export default Hero
+export default Hero;
